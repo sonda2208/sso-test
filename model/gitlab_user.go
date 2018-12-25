b74637cae959c3d37c01d628089321b74970a987
sonda2208 <ace.solix@gmail.com>
2018-12-25 12:38:29 +0700

    Add model structs.

---
+++ model/gitlab_user.go
@@ -0,0 +1,25 @@
+package model
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type GitLabUser struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Login    string `json:"login"`
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+}
+
+func GitLabUserFromJson(data io.Reader) *GitLabUser {
+	decoder := json.NewDecoder(data)
+	var glu GitLabUser
+	err := decoder.Decode(&glu)
+	if err == nil {
+		return &glu
+	}
+
+	return nil
+}
