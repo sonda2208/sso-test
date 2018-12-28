ed84645a63c05d1939ed097bd931698a3a19d322
sonda2208 <ace.solix@gmail.com>
2018-12-28 15:41:11 +0700

    Add store to server struct.

--- app/server.go
+++ app/server.go
@@ -2,15 +2,19 @@ package app
 
 import (
 	"github.com/sonda2208/sso-test/model"
+	"github.com/sonda2208/sso-test/store"
+	"github.com/sonda2208/sso-test/store/mem_store"
 )
 
 type Server struct {
 	config *model.ServiceSettings
+	store  store.TokenStore
 }
 
 func New(conf *model.ServiceSettings) (*Server, error) {
 	s := &Server{
 		config: conf,
+		store:  mem_store.NewOnMemoryTokenStore(),
 	}
 
 	return s, nil
